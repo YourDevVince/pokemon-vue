9d04cbd197d024aaa0e668e69bd28bc3f4743b9c
YourDevVince <divine.visual.coding@gmail.com>
2024-09-26 01:52:57 -0500

    removed the import of option card

--- views/HomeView.vue
+++ views/HomeView.vue
@@ -1,5 +1,4 @@
 <script setup>
-import OptionsCard from '@/components/OptionsCard.vue';
 import OptionsCardV2 from '@/components/OptionsCardV2.vue';
 import ToggleCharacter from '@/components/ToggleCharacter.vue';
 
